125c2e1851e9e0d1348f9f429330a9fccf0513f9
Guram Bautsadze <guram21@gmail.com>
2020-01-11 02:00:57 -0500

    ESLint installed

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,21 +1,19 @@
 const assert = require('assert');
+const {expect} = require('chai');
 
-const {expect} = require('chai').
-
-const {sum, mult} = require('../index');
+const {sum, isAdult, sortArrayUp, sortArrayDown, isPalindrome, isOddNumber, isEvenNumber, isUpperCase, isLowerCase, isInteger} = require('../index');
 
 describe('function sum', () => {
 
-it('should function sum works correct', () => {
+  it('should function sum works correct', () => {
     assert.equal(sum(2, 4), 6);
-})
+  });
 
-it('should function sum not equals 0', () => {
+  it('should function sum not equals 0', () => {
     assert.notEqual(sum(2,4), 0);
-})
-})
+  });
+});
 
-it('should function mult works correct', () => {
-    assert.equal(sum(2, 2), 4);
-})
-
+it('should function sum works correct', () => {
+  assert.equal(sum(2, 2), 4);
+});
