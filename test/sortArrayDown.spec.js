63a7b20ec07c4bc729446be619b31ccce2529f45
Guram Bautsadze <guram21@gmail.com>
2020-01-13 22:14:54 -0500

    Function sortArrayDown tests added

---
+++ test/sortArrayDown.spec.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const {expect} = require('chai');
+const {sortArrayDown} = require('../index');
+
+describe('function sortArrayDown', () => {
+
+  it('should return array sorted in descending order', () => {
+    assert.deepEqual(sortArrayDown([0, 1, 2, 3, 4]), [4, 3, 2, 1, 0]);
+  });
+  // Chai
+  it('should return array sorted in descending order', () =>{
+    expect(sortArrayDown([0, 1, 2, 3, 4])).deep.eq([4, 3, 2, 1, 0]);
+  });
+
+  it('should return an empty array if array is empty', () => {
+    assert.deepEqual(sortArrayDown([]), []);
+  });
+  // Chai
+  it('should return an empty array if array is empty', () => {
+    expect(sortArrayDown([])).deep.eq([]);
+  });
+});
