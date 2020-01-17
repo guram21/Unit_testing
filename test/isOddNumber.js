3678e5dc6898e8cd63d51cc325ec8301d1e395ba
Guram Bautsadze <guram21@gmail.com>
2020-01-17 18:09:21 -0500

    Function isOddNumbers tests added

--- test/isOddNumber.js
+++ test/isOddNumber.js
@@ -1,39 +1,45 @@
-// const assert = require('assert');
-// const {expect} = require('chai');
-// const {isOddNumber} = require('../index');
-//
-// describe('function isOddNumber', () => {
-//   it('should return true if number is odd', () => {
-//     assert.equal(isOddNumber(1), true);
-//   });
-//   it('should return false if the number is not odd', () => {
-//     assert.notEqual(isOddNumber(2), true);
-//   });
-//   it('should return "Enter numeric value" if entry is NaN', () => {
-//     assert.equal(isOddNumber(NaN), 'Enter numeric value');
-//   });
-//   it('should return undefined if not a number', () => {
-//     assert.equal(isOddNumber('str'), undefined);
-//   });
-//   it('should return undefined if empty', () => {
-//     assert.equal(isOddNumber(), undefined);
-//   });
-// });
-//
-// describe('function isOddNumber', () => {
-//   it('should return true if number is odd', () => {
-//     expect(isOddNumber(7)).true;
-//   });
-//   it('should return false if the number is not odd', () => {
-//     expect(isOddNumber(6)).false;
-//   });
-//   it('should return null if x equals 0', () => {
-//     expect(isOddNumber(null)).false;
-//   });
-//   it('should return undefined if not a number', () => {
-//     expect(isOddNumber('str')).undefined;
-//   });
-//   it('should return undefined if empty', () => {
-//     expect(isOddNumber()).undefined;
-//   });
-// });
+const assert = require('assert');
+const {expect} = require('chai');
+const {isOddNumber} = require('../index');
+
+describe('function isOddNumber', () => {
+  it('should return true if entry is odd number', () => {
+    assert.equal(isOddNumber(1), true);
+  });
+  // Chai
+  it('should return true if entry is odd number', () => {
+    expect(isOddNumber(1)).true;
+  });
+
+  it('should return false if entry is not odd number', () => {
+    assert.notEqual(isOddNumber(2), true);
+  });
+  // Chai
+  it('should return false if entry is not odd number', () => {
+    expect(isOddNumber(2)).false;
+  });
+
+  it('should return "Enter numeric value" if entry is NaN', () => {
+    assert.equal(isOddNumber(NaN), 'Enter numeric value');
+  });
+  // Chai
+  it('should return "Enter numeric value" if entry is NaN', () => {
+    expect(isOddNumber(NaN)).eq('Enter numeric value');
+  });
+
+  it('should return "Enter numeric value" if entry is string', () => {
+    assert.equal(isOddNumber('1'), 'Enter numeric value');
+  });
+  // Chai
+  it('should return "Enter numeric value" if entry is string', () => {
+    expect(isOddNumber('1')).eq('Enter numeric value');
+  });
+
+  it('should return "Enter numeric value" if entry is empty', () => {
+    assert.equal(isOddNumber(), 'Enter numeric value');
+  });
+  // Chai
+  it('should return "Enter numeric value" if entry is empty', () => {
+    expect(isOddNumber()).eq('Enter numeric value');
+  });
+});
