ff989608a19573c04dd9fcce5e885ae8111496fa
Guram Bautsadze <guram21@gmail.com>
2020-01-17 20:13:35 -0500

    Function isEvenNumber tests added

---
+++ test/isEvenNumber.spec.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const {expect} = require('chai');
+const {isEvenNumber} = require('../index');
+
+describe('function isOddNumber', () => {
+  it('should return true if value is even number', () => {
+    assert.equal(isEvenNumber(2), true);
+  });
+  // Chai
+  it('should return true if value is even number', () => {
+    expect(isEvenNumber(2)).true;
+  });
+
+  it('should return false if value is not even number', () => {
+    assert.notEqual(isEvenNumber(1), true);
+  });
+  // Chai
+  it('should return false if value is not even number', () => {
+    expect(isEvenNumber(1)).false;
+  });
+
+  it('should return false if value is NaN', () => {
+    assert.notEqual(isEvenNumber(NaN), true);
+  });
+  // Chai
+  it('should return false if value is NaN', () => {
+    expect(isEvenNumber(NaN)).false;
+  });
+
+  it('should return false if value is string', () => {
+    assert.notEqual(isEvenNumber('1'), true);
+  });
+  // Chai
+  it('should return false if value is string', () => {
+    expect(isEvenNumber('1')).false;
+  });
+
+  it('should return false for empty value', () => {
+    assert.notEqual(isEvenNumber(), true);
+  });
+  // Chai
+  it('should return false for empty value', () => {
+    expect(isEvenNumber()).false;
+  });
+});
