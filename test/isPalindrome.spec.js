ddfa6c2e4515bb59eaf5466d2fccc01825161b7f
Guram Bautsadze <guram21@gmail.com>
2020-01-17 20:10:56 -0500

    Function isPalindrome tests added

--- test/isPalindrome.spec.js
+++ test/isPalindrome.spec.js
@@ -4,15 +4,34 @@ const {isPalindrome} = require('../index');
 
 describe('function isPalindrome', () => {
   it('should return true if value is palindrome', () => {
-    assert.equal(isPalindrome('ANNA'), true);
+    assert.equal(isPalindrome('Anna'), true);
   });
+  // Chai
+  it('should return true if value is palindrome', () => {
+    expect(isPalindrome('Anna')).true;
+  });
+
   it('should return false if value is not palindrome', () => {
-    assert.notEqual(isPalindrome('ANNA'), false);
+    assert.notEqual(isPalindrome('Guram'), true);
   });
+  // Chai
+  it('should return false if value is not palindrome', () => {
+    expect(isPalindrome('Guram')).false;
+  });
+
   it('should return false if value is not string', () => {
     assert.notEqual(isPalindrome(123), true);
   });
+  // Chai
+  it('should return false if value is not string', () => {
+    expect(isPalindrome(123)).false;
+  });
+
   it('should return false for empty value',()  => {
     assert.equal(isPalindrome(''), true);
+  });
+  // Chai
+  it('should return false for empty string',()  => {
+    expect(isPalindrome()).false;
   });
 });
