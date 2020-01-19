671dbf1ba96437d924633f309cd4a30141c73749
Guram Bautsadze <guram21@gmail.com>
2020-01-19 17:28:44 -0500

    Function isFirsLetterLowerCase tests added

---
+++ test/isFirsLetterLowerCase.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const {expect} = require('chai');
+const {isFirsLetterLowerCase} = require('../index');
+
+describe('function isFirsLetterLowerCase', () => {
+
+  it('should return true if first letter of value lowercase', () => {
+    assert.equal(isFirsLetterLowerCase('string'), true);
+  });
+  // Chai
+  it('should return true if first letter of value lowercase', () => {
+    expect(isFirsLetterLowerCase('string')).true;
+  });
+
+  it('should return false if first letter of value not lowercase', () => {
+    assert.equal(isFirsLetterLowerCase('String'), false);
+  });
+  // Chai
+  it('should return false if first letter of value not lowercase', () => {
+    expect(isFirsLetterLowerCase('String')).false;
+  });
+
+  it('should return false for not string value', () => {
+    assert.equal(isFirsLetterLowerCase(123), false);
+  });
+  // Chai
+  it('should return false for not string value', () => {
+    expect(isFirsLetterLowerCase(123)).false;
+  });
+
+  it('should return false for empty value',()  => {
+    assert.equal(isFirsLetterLowerCase(''), false);
+  });
+  // Chai
+  it('should return false for empty value',()  => {
+    expect(isFirsLetterLowerCase('')).false;
+  });
+
+});
